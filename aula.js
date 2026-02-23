47f66a20c77fa792b1d39187d21e79853c94f3b3
Celina <f.celina@escola.pr.gov.br>
2026-02-23 12:35:07 -0300

    jjj

---
+++ aula.js
@@ -0,0 +1,9 @@
+// Comentário
+// Criar VARIÁVEL
+let idade = 16; // Tipo Number
+console.log("Idade:", idade);
+
+let nome = "Celina"; // Tipo Stripg
+console.log("Nome:", nome);
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos");
